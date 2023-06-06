022b2ff9d0afa975df2204f04ec2ce62783f6c34
shangjing1990 <135466495+shangjing1990@users.noreply.github.com>
2023-06-06 09:43:11 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -47,7 +47,7 @@ cacheSolve <- function(x,...) {
   }
   m <- solve(x$get())
   x$setinverse(m)
-  #return(m)
+ 
 }
 library(DiagrammeR)
 nodes <- create_node_df(
@@ -76,9 +76,8 @@ m$getinverse()
 cacheSolve(m)
 ## now cacheSolve found out that the inverse in the object is NULL (<<- going one level up) 
 ## and replace the NULL value with the inverse of the matrix
-m$getinverse()
-## now let's set another matrix in the m object
+
 m$set(rbind(sqrt(c(2,2)),c(-1,2)))
 ## notice that every time we enter a new matrix the inverse is turned to NULL with the <<- operator that 
 ## changes the m object's inverse value in the parent frame
-m$getinverse()
+
